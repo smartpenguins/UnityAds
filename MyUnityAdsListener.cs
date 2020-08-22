2769e34b8f46f63ec1d6e4f3f38b7aaff944115e
smartpenguins <70032642+smartpenguins@users.noreply.github.com>
2020-08-21 19:47:12 -0700

    Add files via upload

---
+++ MyUnityAdsListener.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+using Ludiq;
+using Bolt;
+
+public class MyUnityAdsListener : MonoBehaviour, IUnityAdsListener
+{
+    public void OnUnityAdsDidError(string message)
+    {
+        CustomEvent.Trigger(gameObject, "OnUnityAdsDidError", message);
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        CustomEvent.Trigger(gameObject, "OnUnityAdsDidFinish", placementId, showResult);
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+        CustomEvent.Trigger(gameObject, "OnUnityAdsDidStart", placementId);
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+        CustomEvent.Trigger(gameObject, "OnUnityAdsReady", placementId);
+    }
+
+    void Start()
+    {
+        Advertisement.AddListener(this);
+    }
+}
